944fb247a8cb90251f107f89e1c83f0e96c66b00
Viktor Medvid <darkissdark@gmail.com>
2025-05-05 21:33:31 +0200

    Remove unused assets and configuration files from level 1.5

--- level-1.5/eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
